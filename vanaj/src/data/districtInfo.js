7236e8f13297394a1c9d914f62cf68b7a91cadc3
molatile <gobhi666666@gmail.com>
2026-09-16 02:00:33 +0530

    added map

---
+++ vanaj/src/data/districtInfo.js
@@ -0,0 +1,35 @@
+export const languageColors = {
+  Santali: '#E65100', // Deep Vibrant Orange
+  Mundari: '#FFB300', // Vibrant Amber/Yellow
+  Ho: '#2E7D32',      // Deep Tribal Green
+  Kurukh: '#0288D1',  // Bright Blue
+  Kharia: '#C2185B',  // Vibrant Pink/Magenta
+  Khortha: '#5D4037'  // Earthy Brown
+};
+
+export const districtInfo = {
+  Bokaro: { dominantLanguage: 'Khortha', speakers: '1,500,000+', tribes: ['Karmali', 'Munda'], unescoStatus: 'Safe' },
+  Chatra: { dominantLanguage: 'Khortha', speakers: '800,000+', tribes: ['Birhor', 'Oraon'], unescoStatus: 'Safe' },
+  Deoghar: { dominantLanguage: 'Khortha', speakers: '1,200,000+', tribes: ['Santhal'], unescoStatus: 'Safe' },
+  Dhanbad: { dominantLanguage: 'Khortha', speakers: '2,000,000+', tribes: ['Santhal', 'Munda'], unescoStatus: 'Safe' },
+  Dumka: { dominantLanguage: 'Santali', speakers: '1,000,000+', tribes: ['Santhal', 'Paharia'], unescoStatus: 'Safe' },
+  Garhwa: { dominantLanguage: 'Khortha', speakers: '1,100,000+', tribes: ['Korwa', 'Oraon'], unescoStatus: 'Safe' },
+  Giridih: { dominantLanguage: 'Khortha', speakers: '1,800,000+', tribes: ['Santhal', 'Munda'], unescoStatus: 'Safe' },
+  Godda: { dominantLanguage: 'Santali', speakers: '900,000+', tribes: ['Santhal', 'Paharia'], unescoStatus: 'Safe' },
+  Gumla: { dominantLanguage: 'Kurukh', speakers: '700,000+', tribes: ['Oraon', 'Munda', 'Kharia'], unescoStatus: 'Vulnerable' },
+  Hazaribag: { dominantLanguage: 'Khortha', speakers: '1,300,000+', tribes: ['Santhal', 'Munda', 'Birhor'], unescoStatus: 'Safe' },
+  Jamtara: { dominantLanguage: 'Santali', speakers: '600,000+', tribes: ['Santhal'], unescoStatus: 'Safe' },
+  Kodarma: { dominantLanguage: 'Khortha', speakers: '500,000+', tribes: ['Santhal'], unescoStatus: 'Safe' },
+  Latehar: { dominantLanguage: 'Kurukh', speakers: '450,000+', tribes: ['Oraon', 'Kherwar', 'Chero'], unescoStatus: 'Vulnerable' },
+  Lohardaga: { dominantLanguage: 'Kurukh', speakers: '300,000+', tribes: ['Oraon', 'Lohra'], unescoStatus: 'Vulnerable' },
+  Pakur: { dominantLanguage: 'Santali', speakers: '750,000+', tribes: ['Santhal', 'Sauria Paharia'], unescoStatus: 'Safe' },
+  Palamu: { dominantLanguage: 'Khortha', speakers: '1,500,000+', tribes: ['Chero', 'Kharwar', 'Oraon'], unescoStatus: 'Safe' },
+  'Pashchimi Singhbhum': { dominantLanguage: 'Ho', speakers: '900,000+', tribes: ['Ho', 'Munda', 'Bhumij'], unescoStatus: 'Vulnerable' },
+  'Purba Singhbhum': { dominantLanguage: 'Santali', speakers: '1,100,000+', tribes: ['Santhal', 'Ho', 'Bhumij'], unescoStatus: 'Safe' },
+  Ranchi: { dominantLanguage: 'Mundari', speakers: '1,000,000+', tribes: ['Munda', 'Oraon'], unescoStatus: 'Vulnerable' },
+  Sahibganj: { dominantLanguage: 'Santali', speakers: '850,000+', tribes: ['Santhal', 'Mal Paharia'], unescoStatus: 'Safe' },
+  'Saraikela-Kharsawan': { dominantLanguage: 'Ho', speakers: '650,000+', tribes: ['Ho', 'Santhal', 'Bhumij'], unescoStatus: 'Vulnerable' },
+  Simdega: { dominantLanguage: 'Kharia', speakers: '400,000+', tribes: ['Kharia', 'Munda', 'Oraon'], unescoStatus: 'Definitely Endangered' },
+  Khunti: { dominantLanguage: 'Mundari', speakers: '450,000+', tribes: ['Munda'], unescoStatus: 'Vulnerable' },
+  Ramgarh: { dominantLanguage: 'Khortha', speakers: '700,000+', tribes: ['Munda', 'Karmali'], unescoStatus: 'Safe' }
+};
