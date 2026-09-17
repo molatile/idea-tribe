6b7e549ef4be47abe442891246e3a83c1257254f
molatile <gobhi666666@gmail.com>
2026-09-17 12:53:15 +0530

    added tourism -beginner

--- vanaj/src/App.jsx
+++ vanaj/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import Languages from './pages/Languages';
+import Tourism from './pages/Tourism';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Route path="/languages" element={<Languages />} />
           <Route path="/food" element={<div className="pt-24 text-center text-fluid-h2">Food Page Coming Soon</div>} />
           <Route path="/marketplace" element={<div className="pt-24 text-center text-fluid-h2">Marketplace Coming Soon</div>} />
-          <Route path="/tourism" element={<div className="pt-24 text-center text-fluid-h2">Tourism Page Coming Soon</div>} />
+          <Route path="/tourism" element={<Tourism />} />
           <Route path="/archive" element={<div className="pt-24 text-center text-fluid-h2">Archive Coming Soon</div>} />
         </Routes>
       </Layout>
